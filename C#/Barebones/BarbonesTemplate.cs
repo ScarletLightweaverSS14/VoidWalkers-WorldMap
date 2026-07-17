8bdc7c63a5ad426665ff52e2a8aa15240de6f73f
ScarletLightweaver <82061820+ScarletLightweaverSS14@users.noreply.github.com>
2026-07-17 07:13:26 +0200

    Create barebones component template
    Added a template component for SS14 ECS.

---
+++ C#/Barebones/BarbonesTemplate.cs
@@ -0,0 +1,12 @@
+namespace Content.Server._Starlight.LearningCSharp.Components; ///Make sure you read the MD file.
+
+/// <summary>
+/// Template component demonstrating the minimum structure required for a
+/// registered SS14 ECS Component.
+///
+/// This file is intended as a learning reference for creating new Components.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ComponentBarebonesComponent : Component
+{
+}
